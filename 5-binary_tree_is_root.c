2eaa05df4e130bda9d5c60b8722dddce0254a329
sandro132 <mianveo@hotmail.com>
2023-05-19 11:53:27 -0700

    :bug: add file of the point 5

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - check the root
+ *
+ * @node: the root
+ * Return: 1 if is root or 0 otherwise
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node != NULL && !node->parent)
+	{
+		return (1);
+	}
+	return (0);
+}
